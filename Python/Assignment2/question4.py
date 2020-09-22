d6b1f86b011caf2c45b2e8a105ac249706e20c99
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-22 20:41:20 +0530

    Updated Q4

--- Python/Assignment2/question4.py
+++ Python/Assignment2/question4.py
@@ -1,6 +1,6 @@
 str = """Hello, my name is {}. I am {} years old. I live in {}.
 My favourite hobby is {} & my ambition is to be {}.
-My favourite sport is {}. My dream destination is {}"""
+My favourite sport is {}. My dream destination is {}\n"""
 
 print(str.replace('{}', '_____'))
 
@@ -9,4 +9,4 @@ inputs = []
 for i in range(str.count('{}')):
 	inputs.append(input())
 
-print(str.format(inputs[0],inputs[1],inputs[2],inputs[3],inputs[4],inputs[5],inputs[6]))
+print('\n' + str.format(inputs[0],inputs[1],inputs[2],inputs[3],inputs[4],inputs[5],inputs[6]))
